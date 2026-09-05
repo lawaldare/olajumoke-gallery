1618f7560053a0beeca89a4fa8c4b3b677626cf6
lawal <lawal@ebi.ac.uk>
2026-09-05 17:00:09 +0100

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ThiingsGridComponent } from './thiingsGrid';
 import { FormsModule } from '@angular/forms';
 
@@ -17,41 +17,11 @@ import { Pane } from 'tweakpane';
 export class App implements OnInit {
   private readonly modal = inject(NzModalService);
   protected readonly gridSize = 100;
-  protected readonly olajumokePictures = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg',
-    '8.jpg',
-    '9.jpg',
-    '10.jpg',
-    '11.jpg',
-    '12.jpg',
-    '13.jpg',
-    '14.jpg',
-    '15.jpg',
-    '16.jpg',
-    '17.jpg',
-    '18.jpg',
-    '19.jpg',
-    '20.jpg',
-    '21.jpg',
-    '22.jpg',
-    '23.jpg',
-    '24.jpg',
-    '25.jpg',
-    '26.jpg',
-    '27.jpg',
-    '28.jpg',
-    '29.jpg',
-    '30.jpg',
-    '31.jpg',
-    '32.jpg',
-    '33.jpg',
-  ];
+  private readonly imagesCount = signal(46);
+  protected readonly images = computed(() => {
+    const count = this.imagesCount();
+    return Array.from({ length: count }, (_, i) => `${i + 1}.jpg`);
+  });
 
   ngOnInit(): void {
     const pane = new Pane() as any;
